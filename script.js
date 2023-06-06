26e2cdcc95462cae78f3e302fd2a037a56a6a656
Joaquin Marquez <Joacomarquezz33@gmail.com>
2023-06-05 23:13:53 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let edad = Number(prompt("Ingrese su edad"))
+
+if (edad < 6) {
+  alert("Debe pagar 500$ :)")
+} else if (edad < 16) {
+   alert("Debe pagar 800$ :/")
+} else {
+    alert("Debe pagar 1000$ :(")
+}
+
+console.log("FIN PROGRAMA")
